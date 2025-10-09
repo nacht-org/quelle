efe5afd3fcdecb601e7a41c3e3cd3f3587f639a4
Haisham <47662901+m-haisham@users.noreply.github.com>
2025-10-09 19:42:55 +0500

    Add complex search and filters for ScribbleHub

--- extensions/scribblehub/src/lib.rs
+++ extensions/scribblehub/src/lib.rs
@@ -1,6 +1,10 @@
 use eyre::{OptionExt, eyre};
 use once_cell::sync::Lazy;
+use quelle_extension::novel::{ComplexSearchQuery, FilterValue};
+use quelle_extension::source::SortOrder;
 use quelle_extension::{common::time::parse_date_or_relative_time, prelude::*};
+
+mod filters;
 
 register_extension!(Extension);
 
@@ -17,7 +21,9 @@ const META: Lazy<SourceMeta> = Lazy::new(|| SourceMeta {
     capabilities: SourceCapabilities {
         search: Some(SearchCapabilities {
             supports_simple_search: true,
-            ..Default::default()
+            supports_complex_search: true,
+            available_filters: filters::create_filter_definitions(),
+            available_sort_options: filters::create_sort_options(),
         }),
     },
 });
@@ -103,6 +109,277 @@ impl QuelleExtension for Extension {
             .param("sh", query.query)
             .param("order", "desc")
             .param("pg", current_page.to_string())
+            .send(&self.client)
+            .map_err(|e| eyre!(e))?
+            .error_for_status()?;
+
+        let text = response
+            .text()?
+            .ok_or_else(|| eyre!("Failed to get search data"))?;
+
+        let doc = Html::new(&text);
+
+        let mut novels = Vec::new();
+
+        for element in doc.select(".search_main_box")? {
+            let title = element
+                .select_first(".search_title > a")?
+                .text_opt()
+                .ok_or_eyre("Failed to get title")?;
+
+            let url = element
+                .select_first(".search_title > a")?
+                .attr_opt("href")
+                .map(|href| make_absolute_url(&href, BASE_URL))
+                .ok_or_eyre("Failed to get url")?;
+
+            let cover = element
+                .select_first(".search_img > img")?
+                .attr_opt("src")
+                .map(|src| make_absolute_url(&src, BASE_URL));
+
+            novels.push(BasicNovel { title, cover, url });
+        }
+
+        let total_pages = doc
+            .select(".simple-pagination > li")?
+            .into_iter()
+            .filter_map(|li| li.text_opt())
+            .filter_map(|s| s.parse::<u32>().ok())
+            .max()
+            .unwrap_or(current_page);
+
+        Ok(SearchResult {
+            novels,
+            total_count: None,
+            current_page,
+            total_pages: Some(total_pages),
+            has_next_page: current_page < total_pages,
+            has_previous_page: current_page > 1,
+        })
+    }
+
+    fn complex_search(&self, query: ComplexSearchQuery) -> Result<SearchResult, eyre::Report> {
+        let current_page = query.page.unwrap_or(1);
+
+        let mut request =
+            Request::post("https://www.scribblehub.com/series-finder/").param("sf", "1");
+
+        let mut form_builder = RequestFormBuilder::new();
+
+        // Process filters
+        for filter in &query.filters {
+            match filter.filter_id.as_str() {
+                "title_contains" => {
+                    if let FilterValue::Text(value) = &filter.value {
+                        form_builder = form_builder.param("seriescontains", value.as_str());
+                    }
+                }
+                "chapters" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_chapters", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_chapters", max.to_string());
+                        }
+                    }
+                }
+                "releases_perweek" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder =
+                                form_builder.param("min_releases_perweek", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder =
+                                form_builder.param("max_releases_perweek", max.to_string());
+                        }
+                    }
+                }
+                "favorites" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_favorites", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_favorites", max.to_string());
+                        }
+                    }
+                }
+                "ratings" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_ratings", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_ratings", max.to_string());
+                        }
+                    }
+                }
+                "num_ratings" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_num_ratings", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_num_ratings", max.to_string());
+                        }
+                    }
+                }
+                "readers" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_readers", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_readers", max.to_string());
+                        }
+                    }
+                }
+                "reviews" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_reviews", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_reviews", max.to_string());
+                        }
+                    }
+                }
+                "pages" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_pages", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_pages", max.to_string());
+                        }
+                    }
+                }
+                "pageviews" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_pageviews", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_pageviews", max.to_string());
+                        }
+                    }
+                }
+                "totalwords" => {
+                    if let FilterValue::NumberRange(range) = &filter.value {
+                        if let Some(min) = range.min {
+                            form_builder = form_builder.param("min_totalwords", min.to_string());
+                        }
+                        if let Some(max) = range.max {
+                            form_builder = form_builder.param("max_totalwords", max.to_string());
+                        }
+                    }
+                }
+                "genre_mode" => {
+                    if let FilterValue::Select(value) = &filter.value {
+                        form_builder = form_builder.param("gi_mm", value.as_str());
+                    }
+                }
+                "genres_include" => {
+                    if let FilterValue::MultiSelect(values) = &filter.value {
+                        for value in values {
+                            form_builder = form_builder.param("genreselected[]", value.as_str());
+                        }
+                    }
+                }
+                "genres_exclude" => {
+                    if let FilterValue::MultiSelect(values) = &filter.value {
+                        for value in values {
+                            form_builder = form_builder.param("genreexcluded[]", value.as_str());
+                        }
+                    }
+                }
+                "tags_mode" => {
+                    if let FilterValue::Select(value) = &filter.value {
+                        form_builder = form_builder.param("tgi_mm", value.as_str());
+                    }
+                }
+                "tags_include" => {
+                    if let FilterValue::MultiSelect(values) = &filter.value {
+                        for value in values {
+                            form_builder =
+                                form_builder.param("tagsalledit_include[]", value.as_str());
+                        }
+                    }
+                }
+                "tags_exclude" => {
+                    if let FilterValue::MultiSelect(values) = &filter.value {
+                        for value in values {
+                            form_builder =
+                                form_builder.param("tagsalledit_exclude[]", value.as_str());
+                        }
+                    }
+                }
+                "content_warning_mode" => {
+                    if let FilterValue::Select(value) = &filter.value {
+                        form_builder = form_builder.param("ctgi_mm", value.as_str());
+                    }
+                }
+                "content_warnings_include" => {
+                    if let FilterValue::MultiSelect(values) = &filter.value {
+                        for value in values {
+                            form_builder = form_builder.param("ctselected[]", value.as_str());
+                        }
+                    }
+                }
+                "content_warnings_exclude" => {
+                    if let FilterValue::MultiSelect(values) = &filter.value {
+                        for value in values {
+                            form_builder = form_builder.param("ctexcluded[]", value.as_str());
+                        }
+                    }
+                }
+                "story_status" => {
+                    if let FilterValue::Select(value) = &filter.value {
+                        form_builder = form_builder.param("storystatus", value.as_str());
+                    }
+                }
+                "fandom" => {
+                    if let FilterValue::Text(value) = &filter.value {
+                        form_builder = form_builder.param("fandomsearch", value.as_str());
+                    }
+                }
+                "last_update" => {
+                    if let FilterValue::DateRange(range) = &filter.value {
+                        if let Some(start) = &range.start {
+                            form_builder = form_builder.param("dp_release_min", start.as_str());
+                        }
+                        if let Some(end) = &range.end {
+                            form_builder = form_builder.param("dp_release_max", end.as_str());
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        // Handle sorting
+        if let Some(sort_by) = &query.sort_by {
+            form_builder = form_builder.param("sortby", sort_by.as_str());
+        } else {
+            form_builder = form_builder.param("sortby", "pageviews");
+        }
+
+        let sort_order = query.sort_order.unwrap_or(SortOrder::Desc);
+        let order_str = match sort_order {
+            SortOrder::Asc => "asc",
+            SortOrder::Desc => "desc",
+        };
+        form_builder = form_builder.param("order", order_str);
+
+        // Add pagination
+        form_builder = form_builder.param("pg", current_page.to_string());
+
+        request = request.body(form_builder.build());
+
+        let response = request
             .send(&self.client)
             .map_err(|e| eyre!(e))?
             .error_for_status()?;
